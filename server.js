faa054c97407e5408399c3424d991f18b1651ab1
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-14 18:14:27 -0400

    added server.js and start with the pattern learned from the lessons and activities.

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const uuid = require('./helpers/uuid');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+//API routes
+app.get('/api/notes', (req, res) => {
+    
+    res.json(`${req.method} request received to get notes`)
+    
+    console.info(`${req.method} request received to get reviews`)
+    
+    //Read db.json file and return all saved notes as JSON.
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+          const parsedNotes = JSON.parse(data);
+          res.json(parsedNotes);
+        }
+    })
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT}`)
+);
